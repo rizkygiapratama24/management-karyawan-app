a36fd8e762d6e386ec03ea4ae13f53e116876fc2
Rizky Giapratama <rizkygiapratama24@gmail.com>
2024-06-03 02:14:36 +0700

    Update mkr (register vue)

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
